80d6c95271903d6dc6bd7c6689d36df337183154
webchills <office@webchills.com>
2019-07-01 09:51:21 +0200

    Version 3.5.0

---
+++ GEAENDERTE DATEIEN/DEINADMIN/includes/ckeditor.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package admin
+ * @copyright Copyright 2003-2019 Zen Cart Development Team
+ * @copyright Portions Copyright 2010 Kuroi Web Design
+ * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
+ * @version $Id: ckeditor.php for ckfinder 2019-07-01 08:29:32Z webchills $
+ */
+if (!defined('IS_ADMIN_FLAG')) {
+  die('Illegal Access');
+}
+
+$var = zen_get_languages();
+$jsLanguageLookupArray = "var lang = new Array;\n";
+foreach ($var as $key)
+{
+  $jsLanguageLookupArray .= "  lang[" . $key['id'] . "] = '" . $key['code'] . "';\n";
+}
+?>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="includes/javascript/jquery-1.12.4.min.js"><\/script>');</script>
+<script type="text/javascript" src="../<?php echo DIR_WS_EDITORS ?>ckeditor/ckeditor.js"></script>
+<script type="text/javascript" src="../<?php echo DIR_WS_EDITORS ?>ckfinder/ckfinder.js"></script>
+<script type="text/javascript"><!--
+$(document).ready(function() {
+  <?php echo $jsLanguageLookupArray ?>
+  $('textarea').each(function()	{
+    if ($(this).attr('class') == 'editorHook' || ($(this).attr('name') != 'message' && $(this).attr('class') != 'noEditor'))
+    {
+      index = $(this).attr('name').match(/\d+/);
+      if (index == null) index = <?php echo $_SESSION['languages_id'] ?>;
+      var editor = CKEDITOR.replace($(this).attr('name'),
+        {
+          coreStyles_underline : { element : 'u' },
+          width : 760,
+          language: lang[index]
+        });
+      if (CKFinder != undefined) {
+        CKFinder.setupCKEditor( editor, { language: lang[index] }, { type: 'Files' } );
+      }
+    }
+  });
+});
+//--></script>
+<?php 
+// for pseudo auth, use list of admin-approved IP addresses from the Maintenance list:
+  file_put_contents('../' . DIR_WS_EDITORS . '.allowed_ips.txt', EXCLUDE_ADMIN_IP_FOR_MAINTENANCE);
